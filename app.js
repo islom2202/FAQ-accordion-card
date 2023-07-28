ca656d79378c4325df8b88c041ef6fd1b155a7e6
islom2202 <128279879+islom2202@users.noreply.github.com>
2023-07-28 10:36:11 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+const labels = document.querySelectorAll('.label'),
+      labelHeaders = document.querySelectorAll('h3');
+
+labels.forEach((label, index)=>{
+  label.addEventListener('click', ()=>{
+     labels.forEach((remainder) => {
+       remainder.classList.remove("active");
+     });
+    label.classList.toggle("active");
+
+   labelHeaders.forEach((header, hindex) => {
+      if(index === hindex){
+        labelHeaders.forEach((hremainder) => {
+          hremainder.style.color = "var(--dark-grayish-blue)"; //deafult color
+        });
+        header.style.color = "black";
+      }
+   })
+  })
+})
